c08bfd058955b668d252e096d2480e2c930a38b2
zackp30 <zack.piper123@gmail.com>
2014-04-10 17:54:57 +0100

    Resort to an even lower version number...

--- lib/jekyll/version.rb
+++ lib/jekyll/version.rb
@@ -1,3 +1,3 @@
 module Jekyll
-  VERSION = '2.0.0' # Octopress Gemfile does not recognise letters, revert back to this.
+  VERSION = '1.4.3' # Octopress Gemfile does not recognise letters, revert back to this.
 end
